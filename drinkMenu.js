f1cf9fa4b4384b70a031550562977a86568dac9f
mirandaweathers <mirandaweathers@gmail.com>
2022-09-02 21:42:24 -0400

    drink menu file

---
+++ drinkMenu.js
@@ -0,0 +1,29 @@
+const drinkMenu = [
+    {
+        name: 'Gin & Tonic',
+        price: 12,
+        description: 'Hendricks gin with Fever Tree tonic water. Classic and subtly botanical.'
+    },
+    {
+        name: 'Tito\'s & Grapefruit',
+        price: 10,
+        description: 'Tito\'s. Grapefruit. Haley\'s go-to.'
+    },
+    {
+        name: 'Old Fashioned',
+        price: 12,
+        description: 'What\'s in this thing, anyway? Whiskey, I guess?'
+    },
+    {
+        name: 'Miller Lite',
+        price: 6,
+        description: 'Conveniently overpriced for its newfound popularity. It\'s Miller Time.'
+    },
+    {
+        name: 'White Claw',
+        price: 7,
+        description: 'This is the best we could do in terms of seltzers. Sorry'
+    }
+]
+
+module.exports = drinkMenu;
