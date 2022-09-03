63123dc2d5d3846887e179e900c1c1104b9eeeab
mirandaweathers <mirandaweathers@gmail.com>
2022-09-02 21:42:38 -0400

    moved drink menu to own file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config(); // use the .env file
+const menu = require('../drinkMenu.js');
 
 const express = require('express'); // import express library
 const app = express(); // create instance of express app
@@ -15,43 +16,15 @@ const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Now listening on port ${port}`)); // string interpolation
 
-const drinkMenu = [
-    {
-        name: 'Gin & Tonic',
-        price: 12,
-        description: 'Hendricks gin with Fever Tree tonic water. Classic and subtly botanical.'
-    },
-    {
-        name: 'Tito\'s & Grapefruit',
-        price: 10,
-        description: 'Tito\'s. Grapefruit. Haley\'s go-to.'
-    },
-    {
-        name: 'Old Fashioned',
-        price: 12,
-        description: 'What\'s in this thing, anyway? Whiskey, I guess?'
-    },
-    {
-        name: 'Miller Lite',
-        price: 6,
-        description: 'Conveniently overpriced for its newfound popularity. It\'s Miller Time.'
-    },
-    {
-        name: 'White Claw',
-        price: 7,
-        description: 'This is the best we could do in terms of seltzers. Sorry'
-    }
-]
-
 app.get('/drinks', (req, res) => {
     // req = request = all info abt request - cookies, headers, ip address, etc
     // res = response = how the request went and what happened, status message
-    res.send(drinkMenu);
-    console.log(drinkMenu);
+    res.send(menu);
+    console.log(menu);
 })
 
 app.post('/drinks', (req, res) => {
     console.log(req.body);
-    drinkMenu.push(req.body)
+    menu.push(req.body)
     res.sendStatus(201);
 })
